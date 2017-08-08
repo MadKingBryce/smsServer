caf46ff045ccc09aac7bf495c6ce6ad59b17a4fd
Bryce  Greene <bgreene@elevenfiftyconsulting.com>
2017-08-08 19:32:10 -0400

    here

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var http = require('http');	//Add transfer protocol
 var bodyParser = require('body-parser');
 var app = express();
 var router = require('./router');
-
+var cors = require('cors')
 app.use(bodyParser.json({type: '*/*'}))
+
 router(app); 
+app.use(cors())
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+
 
 var port = process.env.PORT || 3000 
 var server = http.createServer(app)
