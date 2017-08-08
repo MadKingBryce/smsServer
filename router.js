005fb880dd7f0d7328c4f6f77a7cbc54e879ad38
Bryce  Greene <bgreene@elevenfiftyconsulting.com>
2017-08-08 19:44:31 -0400

    cors enabled

--- router.js
+++ router.js
@@ -1,11 +1,16 @@
 var sms = require('./sms')
+var cors = require('cors')
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 module.exports = function(app){ //sets routes
 	console.log("Exports?")
 	app.get('/',  function(req, res){ //Adding the requireAuth means that we need a token in order for shit to work. 
 		res.send("Hello")
 	}) 
-	app.post('/sms', function(req, res){
+	app.post('/sms', cors(corsOptions), function(req, res){
 		console.log(req)
 		sms.send(req.body, function(err){
 			if(err){
